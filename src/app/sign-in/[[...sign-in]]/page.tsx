e88f535ac00ca8c24bad0459f336f8f1feef303d
ZiWeiWilly <m3iloveyoum3@gmail.com>
2026-03-05 10:17:11 +0800

    remove Google login from sign-in and sign-up pages

--- src/app/sign-in/[[...sign-in]]/page.tsx
+++ src/app/sign-in/[[...sign-in]]/page.tsx
@@ -3,7 +3,13 @@ import { SignIn } from "@clerk/nextjs";
 export default function SignInPage() {
     return (
         <div className="flex min-h-screen items-center justify-center bg-gray-50 dark:bg-[#09090b]">
-            <SignIn />
+            <SignIn
+                appearance={{
+                    elements: {
+                        socialButtonsBlockButton__google: { display: "none" },
+                    },
+                }}
+            />
         </div>
     );
 }
--- src/app/sign-up/[[...sign-up]]/page.tsx
+++ src/app/sign-up/[[...sign-up]]/page.tsx
@@ -3,7 +3,13 @@ import { SignUp } from "@clerk/nextjs";
 export default function SignUpPage() {
     return (
         <div className="flex min-h-screen items-center justify-center bg-gray-50 dark:bg-[#09090b]">
-            <SignUp />
+            <SignUp
+                appearance={{
+                    elements: {
+                        socialButtonsBlockButton__google: { display: "none" },
+                    },
+                }}
+            />
         </div>
     );
 }
